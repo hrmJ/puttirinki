527978f0483d4ae95f4395f996e3bdfe063e53bc
Juho Härme <juho.harme@gmail.com>
2021-05-08 17:05:21 +0300

    WIP: sketch login component

---
+++ src/lib/CustomButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<button><slot /></button>
+
+<style>
+	button {
+		border: 0;
+		border-radius: 0.25rem;
+		background: #1e88e5;
+		color: white;
+		font-family: -system-ui, sans-serif;
+		font-size: 1rem;
+		line-height: 1.2;
+		white-space: nowrap;
+		text-decoration: none;
+		padding: 0.25rem 0.5rem;
+		margin: 0.25rem;
+		cursor: pointer;
+	}
+</style>
---
+++ src/routes/login.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import CustomButton from '../lib/CustomButton.svelte';
+</script>
+
+<article>
+	<h1>Ja eikun puttailemaan!</h1>
+
+	<form on:submit|preventDefault={() => null}>
+		<p>
+			<label for="email-field">Käyttäjätunnus</label>
+			<input id="email-field" name="email" type="email" />
+		</p>
+
+		<p>
+			<label for="password-field">Salasana</label>
+			<input id="password-field" name="password" type="password" />
+		</p>
+
+		<CustomButton>Kirjaudu</CustomButton>
+	</form>
+</article>
+
+<style>
+	label {
+		display: block;
+	}
+	article {
+		margin: auto;
+		width: 80vw;
+	}
+	form {
+		width: 100%;
+	}
+	input {
+		width: 100%;
+	}
+</style>
