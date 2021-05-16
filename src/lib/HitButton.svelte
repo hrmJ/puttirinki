65be51b88206f98209d9d30b5f73bbe77db644e7
Juho Härme <juho.harme@gmail.com>
2021-05-16 20:24:32 +0300

    add basic styles for the practice component

---
+++ src/lib/HitButton.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	export let heading: string;
+	export let isPrimary: boolean;
+	import { CheckSquareIcon as CheckIcon, XIcon } from 'svelte-feather-icons';
+</script>
+
+<button class={isPrimary ? 'primary' : ''}>
+	{#if isPrimary}
+		<CheckIcon size="3x"/>
+	{:else}
+		<XIcon size="2x" />
+	{/if}
+</button>
+
+<style>
+	button {
+		border: 0;
+		border-radius: 50%;
+		background: hsl(0, 0%, 70%);
+    color: white;
+		font-family: -system-ui, sans-serif;
+		text-decoration: none;
+		padding: 1.2rem 1.2rem;
+		cursor: pointer;
+		font-size: 0.6em;
+		text-transform: uppercase;
+    box-shadow: 0 4px 6px hsla(0,0%,0%,.2);
+	}
+	.primary {
+		padding: 2.2rem 2.2rem;
+		background: hsl(0, 0%, 50%);
+		font-weight: 600;
+		font-size: 1.4em;
+	}
+</style>
--- src/routes/practice.svelte
+++ src/routes/practice.svelte
@@ -1,30 +1,53 @@
+<script lang="ts">
+	import HitButton from '../lib/HitButton.svelte';
+</script>
+
 <div class="grid-container">
-  <div class="topmiddle">Ohi ylhäältä</div>
-  <div class="centermiddle">Osui!</div>
-  <div class="bottommiddle">Ohi alhaalta</div>
-  <div class="rightmiddle">Ohi oikealta</div>
-  <div class="leftmiddle">Ohi vasemmalta</div>
+	<div class="button-container topmiddle">
+		<HitButton heading="Ohi" />
+	</div>
+	<div class="button-container centermiddle"><HitButton heading="Osu" isPrimary /></div>
+	<div class="button-container bottommiddle"><HitButton heading="Ohi" /></div>
+	<div class="button-container rightmiddle"><HitButton heading="Ohi" /></div>
+	<div class="button-container leftmiddle"><HitButton heading="Ohi" /></div>
 </div>
+
 <style type="text/css" media="screen">
-  
-.grid-container {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
-  gap: 0px 0px;
-  grid-template-areas:
-    ". topmiddle . ."
-    "leftmiddle centermiddle rightmiddle ."
-    ". bottommiddle . .";
-}
+	.grid-container {
+		display: grid;
+		height: 95vh;
+		grid-template-columns: 25% 50% 25%;
+		grid-template-rows: 1fr 1fr 1fr;
+		gap: 0px 0px;
+		grid-template-areas:
+			'. topmiddle .'
+			'leftmiddle centermiddle rightmiddle'
+			'. bottommiddle .';
+	}
 
-.topmiddle { grid-area: topmiddle; }
+	.button-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 
-.centermiddle { grid-area: centermiddle; }
+	.topmiddle {
+		grid-area: topmiddle;
+	}
 
-.bottommiddle { grid-area: bottommiddle; }
+	.centermiddle {
+		grid-area: centermiddle;
+	}
 
-.rightmiddle { grid-area: rightmiddle; }
+	.bottommiddle {
+		grid-area: bottommiddle;
+	}
 
-.leftmiddle { grid-area: leftmiddle; }
+	.rightmiddle {
+		grid-area: rightmiddle;
+	}
+
+	.leftmiddle {
+		grid-area: leftmiddle;
+	}
 </style>
